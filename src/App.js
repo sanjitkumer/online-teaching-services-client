f070f4a2d32b333bfa51828c078a6dc32c082ad8
sanjitkumer <sanjitk666@gmail.com>
2022-11-08 23:07:55 +0600

    added header & footer part

--- src/App.js
+++ src/App.js
@@ -4,13 +4,11 @@ import router from './Router/Routes/Routes';
 
 function App() {
 
- 
-
-
   return (
-    <div>
+    <div data-theme="cupcake" className='max-w-screen-xl mx-auto'>
        <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
+
 export default App;
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,45 @@
+import React from 'react';
+// import img1 from '../../../assets/banner/book1.jpg'
+// import img2 from '../../../assets/banner/book2.jpg'
+// import img3 from '../../../assets/banner/book3.jpg'
+// import img4 from '../../../assets/banner/book4.jpg'
+
+
+// const bannerData = [
+//     {
+//         image: img1,
+//         prev:4,
+//         id:1,
+//         next:2
+//     },
+//     {
+//         image: img2,
+//         prev:1,
+//         id:2,
+//         next:3
+//     },
+//     {
+//         image: img3,
+//         prev:2,
+//         id:3,
+//         next:4
+//     },
+//     {
+//         image: img4,
+//         prev:3,
+//         id:4,
+//         next:1
+//     }      
+// ]
+
+
+
+const Banner = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Banner;
