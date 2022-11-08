7b8d72f0de48e9e8779204823080f7b7f93b05ec
sanjitkumer <sanjitk666@gmail.com>
2022-11-08 16:17:40 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { clear } from '@testing-library/user-event/dist/clear';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h2>This is app js</h2>
+    </div>
+  );
+}
+export default App;
