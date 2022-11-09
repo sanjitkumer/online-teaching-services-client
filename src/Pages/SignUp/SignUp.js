19974c0918815c840431bfba61f5a4c13d3ceb38
sanjitkumer <sanjitk666@gmail.com>
2022-11-09 16:26:30 +0600

    added authprovider

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import img from '../../assets/login/images.png'
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 
 
 const SignUp = () => {
+    const {createUser} = useContext(AuthContext)
 
     const handleSignUp = event =>{
         event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        createUser(email, password)
+        .then(result =>{
+          const user = result.user;
+          console.log(user)
+        })
+        .catch(err => console.log(err));
+
     }
 
 
